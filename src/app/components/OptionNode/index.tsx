c4c04fa2b610b0e681c047265948201af0a36005
MurilinhoPs <murilinhorps@gmail.com>
2023-01-22 14:25:49 -0300

    Update optionNode styles

--- src/app/components/OptionNode/index.tsx
+++ src/app/components/OptionNode/index.tsx
@@ -1,28 +1,25 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, memo } from 'react';
 import { OptionNodeData } from '../../services/NodeTypes';
 import { Handle, NodeProps, Position } from 'reactflow';
+import './styles.css'
 
-interface OptionNodeProps {
-    onChange: any;
-    value: string;
-}
-
-const OptionNode = () => {
+const OptionNode: React.FC<NodeProps<OptionNodeData>> = ({ data, xPos, yPos, id }) => {
     const onChange = useCallback((evt: any) => {
         console.log(evt.target.value);
     }, [])
 
-    return <div className='text-node'>
-        <span>nome: </span>
-        <input className='option-input'
+    return <div className='option-node'>
+        <label>nome: </label>
+        <textarea className='option-input'
             id='text'
-            type='text'
+            // type='textArea'
             onChange={onChange}
-            defaultValue='props.value'
+            defaultValue={data.text}
+            rows={2}
         />
         <Handle type='source' position={Position.Right} />
-        <Handle type='source' position={Position.Left} />
+        {/* <Handle type='source' position={Position.Left} /> */}
     </div>
 }
 
-export default OptionNode;
+export default memo(OptionNode);
